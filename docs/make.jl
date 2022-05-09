a8cb763be7316d3dce197b153f27f0b704f60ffd
Mateusz Baran <mateuszbaran89@gmail.com>
2022-05-09 11:52:27 +0200

    docs & other

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,12 @@
 using Plots, Manifolds, ManifoldsBase, ManifoldDiff, Documenter, PyPlot
 ENV["GKSwstype"] = "100"
+# required for loading the manifold tests functions
+using Test, ForwardDiff, ReverseDiff, FiniteDifferences
 
 makedocs(
     # for development, we disable prettyurls
     format = Documenter.HTML(prettyurls = false, assets = ["assets/favicon.ico"]),
-    modules = [ManifoldDiffDev],
+    modules = [ManifoldDiff],
     authors = "Seth Axen, Mateusz Baran, Ronny Bergmann, and contributors.",
     sitename = "ManifoldDiff.jl",
     pages = ["Home" => "index.md", "Library" => "library.md"],
--- docs/src/library.md
+++ docs/src/library.md
@@ -1,1 +1,19 @@
 # Different library functions
+
+Documentation for `ManifoldDiff.jl`'s methods and types for finite differences and automatic differentiation.
+
+### ForwardDiff.jl
+
+```@autodocs
+Modules = [ManifoldDiff]
+Pages = ["forward_diff.jl"]
+Order = [:type, :function, :constant]
+```
+
+### FiniteDifferenes.jl
+
+```@autodocs
+Modules = [ManifoldDiff]
+Pages = ["finite_differences.jl"]
+Order = [:type, :function, :constant]
+```
