b167b8a94096f75fbd48bcffd38838964617d141
Ronny Bergmann <git@ronnybergmann.net>
2023-02-11 21:41:52 +0100

    add Number and formatting.

--- src/derivatives.jl
+++ src/derivatives.jl
@@ -1,6 +1,6 @@
 @doc raw"""
-    Y = geodesic_derivative(M, p, X, t; γt = geodesic(M, p, X, t))
-    geodesic_derivative!(M, Y, p, X, t; γt = geodesic(M, p, X, t))
+    Y = geodesic_derivative(M, p, X, t::Number; γt = geodesic(M, p, X, t))
+    geodesic_derivative!(M, Y, p, X, t::Number; γt = geodesic(M, p, X, t))
 
 Evaluate the derivative of the geodesic ``γ(t)`` with ``γ_{p,X}(0) = p`` and ``\dot γ_{p,X}(0) = X`` at ``t``.
 The formula reads
@@ -17,17 +17,17 @@ This computation can also be done in-place of ``Y``.
 * `γt` – (`geodesic(M, p, X, t)`) the point on the geodesic at ``t``.
   This way if the point was computed earlier it can be resued here.
 """
-function geodesic_derivative(M, p, X, t; q = geodesic(M, p, X, t))
+function geodesic_derivative(M, p, X, t::Number; q = geodesic(M, p, X, t))
     return parallel_transport_to(M, p, X, q)
 end
-function geodesic_derivative!(M, Y, p, X, t; q = geodesic(M, p, X, t))
+function geodesic_derivative!(M, Y, p, X, t::Number; q = geodesic(M, p, X, t))
     parallel_transport_to!(M, Y, p, X, q)
     return Y
 end
 
 @doc raw"""
-    Y = shortest_geodesic_derivative(M, p, X, t; γt = shortest_geodesic(M, p, q, t))
-    shortest_geodesic_derivative!(M, Y, p, X, t; γt = shortest_geodesic(M, p, q, t))
+    Y = shortest_geodesic_derivative(M, p, X, t::Number; γt = shortest_geodesic(M, p, q, t))
+    shortest_geodesic_derivative!(M, Y, p, X, t::Number; γt = shortest_geodesic(M, p, q, t))
 
 Evaluate the derivative of the shortest geodesic ``γ(t)`` with ``γ_{p,q}(0) = p`` and ``\dot γ_{p,q}(1) = q``
 at ``t``.
@@ -42,13 +42,13 @@ This computation can also be done in-place of ``Y``.
 
 # Optional Parameters
 
-* `γt = geodesic(M, p,X,t)` the point on the geodesic at ``t``.
+* `γt = geodesic(M, p, X, t)` the point on the geodesic at ``t``.
   This way if the point was computed earlier it can be resued here.
 """
-function shortest_geodesic_derivative(M, p, q, t; γt = shortest_geodesic(M, p, q, t))
+function shortest_geodesic_derivative(M, p, q, t::Number; γt = shortest_geodesic(M, p, q, t))
     return parallel_transport_to(M, p, log(M, p, q), γt)
 end
-function shortest_geodesic_derivative!(M, Y, p, q, t; γt = shortest_geodesic(M, p, q, t))
+function shortest_geodesic_derivative!(M, Y, p, q, t::Number; γt = shortest_geodesic(M, p, q, t))
     log!(M, Y, p, q)
     parallel_transport_to!(M, Y, p, Y, γt)
     return Y
