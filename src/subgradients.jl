70a22fd014a131ff628b49dfce49930d3c5b6eb0
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-07 16:34:46 +0200

    Runs formatter

--- src/subgradients.jl
+++ src/subgradients.jl
@@ -16,13 +16,13 @@ The subgradient at `p = q`is given by a norm one tangent vector at `p`.
 
 * `atol` – (`eps(eltype(p))`) the tolerance to use when evaluating the distance between `p` and `q`.
 """
-function subgrad_distance(M, q, p; atol=eps(eltype(p)))
+function subgrad_distance(M, q, p; atol = eps(eltype(p)))
     @assert distance(M, q, p) ≤ atol ["Since p and q are different, use grad_distance."]
     X = rand(M; vector_at = p)
     (norm(M, p, X) > 1.0) && (X ./= norm(M, p, X))
     return X
 end
-function subgrad_distance!(M, X, q, p; atol=eps(eltype(p)))
+function subgrad_distance!(M, X, q, p; atol = eps(eltype(p)))
     c = check_vector(M, p, X; atol = atol)
     if c === nothing
         @assert distance(M, q, p) ≤ atol ["Since p and q are different, use grad_distance."]
