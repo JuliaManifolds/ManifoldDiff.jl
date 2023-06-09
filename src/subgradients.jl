a7db8322c1dfd3d860698a46e8d230b1fd14bf83
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-09 16:11:58 +0200

    Changes standard exponent to 2 and uses normal_cone_vector function

--- src/subgradients.jl
+++ src/subgradients.jl
@@ -24,26 +24,35 @@ for ``c\neq 1`` or ``p\neq  q``. Note that for the remaining case ``c=1``,
 * `c` – (`1`) the exponent of the distance,  i.e. the default is the distance
 * `atol` – (`eps(eltype(p))`) the tolerance to use when evaluating the distance between `p` and `q`.
 """
-function subgrad_distance(M, q, p; c::Int = 1, atol = 0)
+function subgrad_distance(M, q, p; c::Int = 2, atol = 0)
     if c == 2
         return -log(M, p, q)
     elseif c == 1 && distance(M, q, p) ≤ atol
-        X = rand(M; vector_at = p)
-        (norm(M, p, X) > 1.0) && (X ./= norm(M, p, X))
-        return X
+        return normal_cone_vector(M, p)
     else
         return -distance(M, p, q)^(c - 2) * log(M, p, q)
     end
 end
-function subgrad_distance!(M, X, q, p; c::Int = 1, atol = 0)
+function subgrad_distance!(M, X, q, p; c::Int = 2, atol = 0)
     log!(M, X, p, q)
     if c == 2
         X .*= -one(eltype(X))
     elseif c == 1 && distance(M, q, p) ≤ atol
-        ManifoldsBase.rand!(M, X; vector_at = p)
-        (norm(M, p, X) > 1.0) && (X ./= norm(M, p, X))
+        normal_cone_vector!(M, X, p)
     else
         X .*= -distance(M, p, q)^(c - 2)
     end
     return X
+end
+function normal_cone_vector(M, p)
+    Y = rand(M; vector_at = p)
+    (norm(M, p, Y) > 1.0) && (Y ./= norm(M, p, Y))
+    Y .*= rand()
+    return Y
+end
+function normal_cone_vector!(M, Y, p)
+    ManifoldsBase.rand!(M, Y; vector_at=p)
+    (norm(M, p, Y) > 1.0) && (Y ./= norm(M, p, Y))
+    Y .*= rand()
+    return Y
 end
