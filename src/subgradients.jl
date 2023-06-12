d7c96cc61fd6c8db3fdffec583a9c053f5987e7c
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-12 09:43:17 +0200

    Change default exponent to 2 and make it positional

--- src/subgradients.jl
+++ src/subgradients.jl
@@ -1,7 +1,7 @@
 
 @doc raw"""
-    subgrad_distance(M, q, p; c = 1, atol=eps(eltype(p))])
-    subgrad_distance!(M, X, q, p; c = 1, atol=eps(eltype(p))])
+    subgrad_distance(M, q, p[, c = 1; atol = 0])
+    subgrad_distance!(M, X, q, p[, c = 1; atol = 0])
 
 compute the subgradient of the distance (in place of `X`)
 
@@ -24,7 +24,7 @@ for ``c\neq 1`` or ``p\neq  q``. Note that for the remaining case ``c=1``,
 * `c` – (`1`) the exponent of the distance,  i.e. the default is the distance
 * `atol` – (`eps(eltype(p))`) the tolerance to use when evaluating the distance between `p` and `q`.
 """
-function subgrad_distance(M, q, p; c::Int = 2, atol = 0)
+function subgrad_distance(M, q, p, c::Int = 2; atol = 0)
     if c == 2
         return -log(M, p, q)
     elseif c == 1 && distance(M, q, p) ≤ atol
@@ -33,7 +33,7 @@ function subgrad_distance(M, q, p; c::Int = 2, atol = 0)
         return -distance(M, p, q)^(c - 2) * log(M, p, q)
     end
 end
-function subgrad_distance!(M, X, q, p; c::Int = 2, atol = 0)
+function subgrad_distance!(M, X, q, p, c::Int = 2; atol = 0)
     log!(M, X, p, q)
     if c == 2
         X .*= -one(eltype(X))
