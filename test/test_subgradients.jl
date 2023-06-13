f20208365fe0bd764e02b74d492303e5e053a605
Hajg Jasa <hajg.jasa@ntnu.no>
2023-06-13 09:56:07 +0200

    Changes tests to accommodate for default exponent change

--- test/test_subgradients.jl
+++ test/test_subgradients.jl
@@ -11,25 +11,30 @@ using Random
         w = p
         X = zero_vector(M, w)
         Random.seed!(42)
-        ManifoldDiff.subgrad_distance!(M, X, w, p)
-        Y = ManifoldDiff.subgrad_distance(M, w, p)
+        ManifoldDiff.subgrad_distance!(M, X, w, p, 1)
+        Random.seed!(42)
+        Y = ManifoldDiff.subgrad_distance(M, w, p, 1)
+        @test Y == X
         @test is_vector(M, p, X)
         @test norm(M, p, X) ≤ 1.0
         @test is_vector(M, p, Y)
         @test norm(M, p, Y) ≤ 1.0
         Z = ManifoldDiff.grad_distance(M, p, r)
+        W = ManifoldDiff.subgrad_distance(M, p, r)
+        @test Z == W
+        Z = ManifoldDiff.grad_distance(M, p, r, 2)
         W = ManifoldDiff.subgrad_distance(M, p, r, 2)
         @test Z == W
 
         X = zero_vector(M, p)
-        ManifoldDiff.subgrad_distance!(M, X, p, q, 2)
-        Y = ManifoldDiff.subgrad_distance(M, p, q, 2)
+        ManifoldDiff.subgrad_distance!(M, X, p, q)
+        Y = ManifoldDiff.subgrad_distance(M, p, q)
         Z = [0.0, 0.0, -π / 2] # known solution
         @test X == Y
         @test X == Z
-        U = zero_vector(M, p)
-        ManifoldDiff.subgrad_distance!(M, U, p, q)
-        V = ManifoldDiff.subgrad_distance(M, p, q)
+        U = zero_vector(M, q)
+        ManifoldDiff.subgrad_distance!(M, U, p, q, 1)
+        V = ManifoldDiff.subgrad_distance(M, p, q, 1)
         W = -distance(M, q, p)^(-1) * log(M, q, p) # solution
         @test U == V
         @test U == W
