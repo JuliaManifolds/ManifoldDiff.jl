aa501c8509f2900f631f4a3b2e57e915b143e781
Ronny Bergmann <git@ronnybergmann.net>
2023-12-04 10:35:00 +0100

    Unify variables and docs.

--- src/proximal_maps.jl
+++ src/proximal_maps.jl
@@ -1,50 +1,48 @@
 @doc raw"""
-    y = prox_distance(M,λ,f,x [, p=2])
-    prox_distance!(M, y, λ, f, x [, p=2])
+    y = prox_distance(M, λ, f, p [, r=2])
+    prox_distance!(M, q, λ, f, p [, r=2])
 
-compute the proximal map ``\operatorname{prox}_{λ\varphi}`` with
-parameter λ of ``φ(x) = \frac{1}{p}d_{\mathcal M}^p(f,x)``.
-For the mutating variant the computation is done in place of `y`.
+compute the proximal map ``\operatorname{prox}_{λφ}`` with
+parameter λ of ``φ(p) = \frac{1}{r}d_{\mathcal M}^r(f,p)``.
+For the in-place variant the computation is done in place of `q`.
 
 # Input
 * `M` – a manifold `M`
 * `λ` – the prox parameter
 * `f` – a point ``f ∈ \mathcal M`` (the data)
-* `x` – the argument of the proximal map
-
-# Optional argument
-* `p` – (`2`) exponent of the distance.
+* `p` – the argument of the proximal map
+* `r` – (`2`) exponent of the distance.
 
 # Output
-* `y` – the result of the proximal map of ``φ``
+* `q` – the result of the proximal map of ``φ``
 """
-function prox_distance(M::AbstractManifold, λ, f, x, p::Int = 2)
-    d = distance(M, f, x)
-    if p == 2
+function prox_distance(M::AbstractManifold, λ, f, p, r::Int = 2)
+    d = distance(M, f, p)
+    if r == 2
         t = λ / (1 + λ)
-    elseif p == 1
+    elseif r == 1
         t = (λ < d) ? λ / d : 1.0
     else
         throw(
             ErrorException(
-                "Proximal Map of distance(M,f,x) not implemented for p=$(p) (requires p=1 or 2)",
+                "Proximal Map of distance(M,f,x) not implemented for an exponent $(r) (requires 1 or 2)",
             ),
         )
     end
-    return exp(M, x, log(M, x, f), t)
+    return exp(M, p, log(M, p, f), t)
 end
-function prox_distance!(M::AbstractManifold, y, λ, f, x, p::Int = 2)
-    d = distance(M, f, x)
-    if p == 2
+function prox_distance!(M::AbstractManifold, q, λ, f, p, r::Int = 2)
+    d = distance(M, f, p)
+    if r == 2
         t = λ / (1 + λ)
-    elseif p == 1
+    elseif r == 1
         t = (λ < d) ? λ / d : 1.0
     else
         throw(
             ErrorException(
-                "Proximal Map of distance(M,f,x) not implemented for p=$(p) (requires p=1 or 2)",
+                "Proximal Map of distance(M,f,x) not implemented for an exponent $(r) (requires 1 or 2)",
             ),
         )
     end
-    return exp!(M, y, x, log(M, x, f), t)
+    return exp!(M, q, p, log(M, p, f), t)
 end
