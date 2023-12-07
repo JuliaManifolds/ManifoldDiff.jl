bf482f11573b18b239ff3e2a9babbccd35458a51
Ronny Bergmann <git@ronnybergmann.net>
2023-12-07 11:32:11 +0100

    unify notation.

--- src/proximal_maps.jl
+++ src/proximal_maps.jl
@@ -1,25 +1,25 @@
 @doc raw"""
-    y = prox_distance(M, λ, f, p [, r=2])
-    prox_distance!(M, q, λ, f, p [, r=2])
+    y = prox_distance(M, λ, p_data, p [, r=2])
+    prox_distance!(M, q, λ, p_data, p [, r=2])
 
-Compute the proximal map ``\operatorname{prox}_{λφ}`` with
-parameter λ of ``φ(p) = \frac{1}{r}d_{\mathcal M}^r(f,p)``.
+Compute the proximal map ``\operatorname{prox}_{λf}`` with
+parameter λ of ``f(p) = \frac{1}{r}d_{\mathcal M}^r(p_data,p)``.
 For the in-place variant the computation is done in place of `q`.
 
 # Input
-* `M` – a manifold `M`
-* `λ` – the prox parameter, a positive real number.
-* `f` – a point ``f ∈ \mathcal M`` (the data)
-* `p` – the argument of the proximal map
-* `r` – (`2`) exponent of the distance.
+* `M`      a manifold `M`
+* `λ`      the prox parameter, a positive real number.
+* `p_data` a point on `M`.
+* `p`      the argument of the proximal map
+* `r`      (`2`) exponent of the distance.
 
 # Output
 * `q` – the result of the proximal map of ``φ``
 
 For more details see [WeinmannDemaretStorath:2014](@cite)
 """
-function prox_distance(M::AbstractManifold, λ::Real, f, p, r::Int = 2)
-    d = distance(M, f, p)
+function prox_distance(M::AbstractManifold, λ::Real, p_data, p, r::Int = 2)
+    d = distance(M, p_data, p)
     if r == 2
         t = λ / (1 + λ)
     elseif r == 1
@@ -31,10 +31,10 @@ function prox_distance(M::AbstractManifold, λ::Real, f, p, r::Int = 2)
             ),
         )
     end
-    return shortest_geodesic(M, p, f, t)
+    return shortest_geodesic(M, p, p_data, t)
 end
-function prox_distance!(M::AbstractManifold, q, λ, f, p, r::Int = 2)
-    d = distance(M, f, p)
+function prox_distance!(M::AbstractManifold, q, λ, p_data, p, r::Int = 2)
+    d = distance(M, p_data, p)
     if r == 2
         t = λ / (1 + λ)
     elseif r == 1
@@ -46,5 +46,5 @@ function prox_distance!(M::AbstractManifold, q, λ, f, p, r::Int = 2)
             ),
         )
     end
-    return shortest_geodesic!(M, q, p, f, t)
+    return shortest_geodesic!(M, q, p, p_data, t)
 end
